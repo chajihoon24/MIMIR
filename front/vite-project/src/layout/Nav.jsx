c1544eef2b7382c816f35564040db045ae187ece
DESKTOP-DAIRV42\차지훈 <jihoon9998@naver.com>
2025-12-29 22:45:18 +0900

    tailwind

--- front/vite-project/src/layout/Nav.jsx
+++ front/vite-project/src/layout/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav>
-      <ul className="nav__container">
+      <ul className="nav__container bg">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/page1">Page1</Link></li>
         <li><Link to="/page2">Page2</Link></li>
